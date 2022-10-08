bf5b29cd0cf1f804270bf7316b2d2fa7ee9e4662
Sumit Kumar <69559993+DigitalMachine01@users.noreply.github.com>
2022-10-08 19:21:19 +0530

    Exercise 1.3

--- fahrCelsius.c
+++ fahrCelsius.c
@@ -4,17 +4,19 @@
     for fahr = 0, 20, ..., 300 */
 int main()
 {
-    int fahr, celsius;
-    int lower, upper, step;
+    float fahr, celsius;
+    float lower, upper, step;
 
     lower = 0;      /* lower limit of temperature scale */
     upper = 300;    /* upper limit */
     step = 20;      /* step size */
 
     fahr = lower;
+
+    printf("Temp(F)\tTemp(C)\n");
     while (fahr <= upper) {
-        celsius = 5 * (fahr - 32) / 9;
-        printf("%3d%6d\n", fahr, celsius);
+        celsius = 5.0/9.0 * (fahr - 32.0);
+        printf("%3.0f\t%6.1f\n", fahr, celsius);
         fahr = fahr + step;
     }
 }
