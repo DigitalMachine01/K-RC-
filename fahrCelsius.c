b93f312950891ebf69e9686ff8f3efd88a35b35c
Sumit Kumar <69559993+DigitalMachine01@users.noreply.github.com>
2022-10-09 07:26:39 +0530

    table reversed

--- fahrCelsius.c
+++ fahrCelsius.c
@@ -11,12 +11,12 @@ int main()
     upper = 100;    /* upper limit */
     step = 10;      /* step size */
 
-    celsius = lower;
+    celsius = upper;
 
     printf("Temp(C)\tTemp(F)\n");
-    while (celsius <= upper) {
+    while (celsius >= lower) {
         fahr = (9.0/5.0) * celsius + 32.0;
         printf("%3.0f\t%6.1f\n", celsius, fahr);
-        celsius = celsius + step;
+        celsius = celsius - step;
     }
 }
